6518df3c6140cf7130bde6c1fbe222c6af17959e
Vincent <sggamecard@gmail.com>
2022-10-17 00:03:29 +0800

    use assume_role_with_web_identity

--- dags/hello_world.py
+++ dags/hello_world.py
@@ -2,13 +2,43 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
-import boto3
+import boto3, os
 
 print("init s3...")
-s3_client = boto3.client('s3', region_name='ap-southeast-1')
+sts_client = boto3.client('sts')
+with open(os.getenv("AWS_WEB_IDENTITY_TOKEN_FILE")) as f:
+    web_token = f.readlines()
+response = sts_client.assume_role_with_web_identity(
+    DurationSeconds=3600,
+    RoleArn=os.getenv("AWS_ROLE_ARN"),
+    RoleSessionName='dag',
+    WebIdentityToken=web_token
+)
+credentials = response['Credentials']
+
+s3_client = boto3.client('s3', region_name='ap-southeast-1', 
+        aws_access_key_id=credentials['AccessKeyId'],
+        aws_secret_access_key=credentials['SecretAccessKey'],
+        aws_session_token=credentials['SessionToken'],
+    )
 for key in s3_client.list_objects(Bucket='fargate-airflow-logs')['Contents']:
     print(key['Key'])
 print("s3 initilized...")
+
+
+
+
+
+    s3_resource = boto3.resource(
+        's3',
+        
+
+    for bucket in s3_resource.buckets.all():
+        print(bucket.name)
+
+
+
+
 
 def print_hello():
     return 'Hello world from first Airflow DAG!'
